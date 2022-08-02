a32083fae85b6676ead8d90fe48524be81821eba
Tony Messias <tonysm@hey.com>
2022-08-01 22:51:12 -0300

    Adds the std library for Stimulus controller

--- src/Commands/Concerns/InstallsForImportmap.php
+++ src/Commands/Concerns/InstallsForImportmap.php
@@ -44,13 +44,15 @@ trait InstallsForImportmap
             'packages' => ['@hotwired/stimulus'],
         ]);
 
-        // Publishes the `@hotwired/stimulus-loading` package to public/
+        // Publishes the `@hotwired/stimulus-loading` and the std controllers to public/
         $this->callSilently('vendor:publish', [
             '--tag' => 'stimulus-laravel-assets',
         ]);
 
         File::append($this->importmapsFile(), <<<'IMPORTMAP'
         Importmap::pin("@hotwired/stimulus-loading", to: "vendor/stimulus-laravel/stimulus-loading.js", preload: true);
+        Importmap::pinAllFrom("public/vendor/stimulus-laravel/controllers", to: "vendor/stimulus-laravel/controllers", under: "std", preload: true);
+
         IMPORTMAP);
     }
 }
