f8a751388757a72aa728b34c273a8be24342cacc
Tony Messias <tonysm@hey.com>
2025-05-07 21:28:27 -0300

    Adds the optional bridge command to the core make command

--- src/Commands/CoreMakeCommand.php
+++ src/Commands/CoreMakeCommand.php
@@ -4,5 +4,5 @@ namespace HotwiredLaravel\StimulusLaravel\Commands;
 
 class CoreMakeCommand extends MakeCommand
 {
-    public $signature = 'make:stimulus {name : The Controller name}';
+    public $signature = 'make:stimulus {name : The Controller name} {--bridge= : The name of the bridge component to be created}';
 }
